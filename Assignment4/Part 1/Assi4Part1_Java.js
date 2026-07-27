c4b2cac513d8830b0859cc5eebcb0f167c37d174
Hari522597 <harithanraveendran@gmail.com>
2026-07-26 20:25:52 -0400

    Fixed bugs regarding the incorrect usage of brackets.

--- Assignment4/Part 1/Assi4Part1_Java.js
+++ Assignment4/Part 1/Assi4Part1_Java.js
@@ -10,9 +10,9 @@ function randomValueFromArray(array) {
 }
 
 // Raw text strings
-const characters = {"Willy the Goblin", "Big Daddy", "Father Christmas"}
-const places = {"the soup kitchen", "Disneyland", "the White House"}
-const events = {"spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"}
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
 // Partial return random string function
 
@@ -21,7 +21,7 @@ function returnRandomStoryString() {
   const randomPlace = randomValueFromArray(places);
   const randomEvent = randomValueFromArray(events);
   
-  let storyText = "It was 94 Fahrenheit outside, so :${randomCharacter}: went for a walk. When they got to {randomPlace}, they stared in horror for a few moments, then :${randomEvents}:. Bob saw the whole thing, but was not surprised — :${randomCharacter}: weighs 300 pounds, and it was a hot day."
+  let storyText = "It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvents}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day."
   return storyText;
 }
 
